85387d6be4ccff8782d9074a8d5468c113b1a8ba
Narupo <pouzumaki@gmail.com>
2025-01-13 23:46:02 -0600

    updated nav.tsx, accidently named branch poorly.

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,7 +1,22 @@
+import { NavLink } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <nav className="nav">
+      <ul>
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link" activeClassName="active">
+            Search Candidates
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/savedCandidates" className="nav-link" activeClassName="active">
+            Saved Candidates
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 };
 
